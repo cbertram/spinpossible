ab138099622e08d5922bda6c949152a17c17c732
Christian Bertram <cbertram@protonmail.com>
2018-10-02 00:18:14 +0200

    Add touch support (try 3)

--- script.js
+++ script.js
@@ -188,4 +188,18 @@ window.onload = function() {
             clientY: event.touches[0].clientY
         }));
     }, false);
+
+    // Prevent scrolling when touching the canvas
+    document.body.addEventListener("touchstart", function (e) {
+        if (e.target == canvas)
+            e.preventDefault();
+    }, false);
+    document.body.addEventListener("touchend", function (e) {
+        if (e.target == canvas)
+            e.preventDefault();
+    }, false);
+    document.body.addEventListener("touchmove", function (e) {
+        if (e.target == canvas)
+            e.preventDefault();
+    }, false);
 };
